db72f0ba3b15b7ec53b89f78e13cb99c20ab8a8f
Luciane <lucimlozano@gmail.com>
2024-01-18 21:54:12 -0300

    tela login VSConnect kotlin

--- app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
@@ -2,8 +2,13 @@ package com.senai.vsconnect_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//databinding pega info do layout.
+import com.senai.vsconnect_kotlin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    //é uma propriedade privada como o nome binding do tipo activityloginBinding
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
