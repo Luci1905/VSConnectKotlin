0f45500fbe203ad185cd31244e1a265a7fa01fb6
Luciane <lucimlozano@gmail.com>
2024-01-19 21:55:01 -0300

    tela servicos kotlin

--- app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.senai.vsconnect_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //databinding pega info do layout.
@@ -11,6 +12,22 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //Atribui à variável binding um objeto que contem referencias(propriedades)
+        // aos elementos definidos no layout
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding.btnEntrar.setOnClickListener {
+            //variável mainIntent com a intençãode sair do LoginActivity e ir para MainActivity
+            val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+
+            //executa a intenção armazenada na variável mainTntent
+            startActivity(mainIntent)
+
+            //finaliza a loginActivity
+            finish()
+        }
+
+        setContentView(binding.root)
+
     }
 }
