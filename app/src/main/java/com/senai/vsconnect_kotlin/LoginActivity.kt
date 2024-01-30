0f93287e39d8a7b175a0f3984f71f9ea53438e02
Luciane <lucimlozano@gmail.com>
2024-01-29 21:25:36 -0300

    termino da tela login em Kotlin

--- app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
@@ -1,17 +1,34 @@
 package com.senai.vsconnect_kotlin
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import com.google.gson.JsonObject
+import com.senai.vsconnect_kotlin.apis.EndpointInterface
+import com.senai.vsconnect_kotlin.apis.RetrofitConfig
 //databinding pega info do layout.
 import com.senai.vsconnect_kotlin.databinding.ActivityLoginBinding
 import com.senai.vsconnect_kotlin.models.Login
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.create
+import java.nio.charset.StandardCharsets
+import java.util.Base64
 
 class LoginActivity : AppCompatActivity() {
     //é uma propriedade privada como o nome binding do tipo activityloginBinding
     private lateinit var binding: ActivityLoginBinding
+
+    private val clienteRetrofit = RetrofitConfig.obterInstanciaRetrofit()
+
+    private val endpoints = clienteRetrofit.create(EndpointInterface::class.java)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,22 +36,23 @@ class LoginActivity : AppCompatActivity() {
         // aos elementos definidos no layout
         binding = ActivityLoginBinding.inflate(layoutInflater)
 
+        val sharedPreferences = getSharedPreferences("idUsuario", Context.MODE_PRIVATE)
+
+        val editor = sharedPreferences.edit()
+
+        editor.remove("idUsuario")
+
+        editor.apply()
+
         binding.btnEntrar.setOnClickListener {
-            //variável mainIntent com a intençãode sair do LoginActivity e ir para MainActivity
-            val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
-
-            //executa a intenção armazenada na variável mainTntent
-            startActivity(mainIntent)
-
-            //finaliza a loginActivity
-            finish()
+            autenticarUsuario()
         }
 
         setContentView(binding.root)
 
     }
 
-    private fun autenticarUsuario() {}
+    private fun autenticarUsuario() {
         val root: View = binding.root
 
         val idEmail = root.findViewById<EditText>(R.id.campo_email)
@@ -44,4 +62,55 @@ class LoginActivity : AppCompatActivity() {
         val senhaDigitado = idSenha.text.toString()
 
         val usuario = Login(emailDigitado, senhaDigitado)
+
+        endpoints.login(usuario).enqueue(object : Callback<JsonObject>{
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                when(response.code()){
+                    200 -> {
+                        val idUsuario = decodificarToken(response.body().toString())
+
+                        val sharedPreferences = getSharedPreferences("idUsuario", Context.MODE_PRIVATE)
+
+                        val editor = sharedPreferences.edit()
+
+                        editor.putString("idUsuario", idUsuario.toString())
+
+                        editor.apply()
+
+                        //direcionando o usuario para tela lista de servico
+                        val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+
+                        startActivity(mainIntent)
+
+                        finish()
+                    }
+                    403 -> { tratarFalhaNaAutenticacao("Email e/ou senha inválidos.") }
+                    else -> { tratarFalhaNaAutenticacao("Falha ao se logar!") }
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                tratarFalhaNaAutenticacao("Falha ao tentar se logar!")
+            }
+
+        })
+
+    }
+
+    private fun tratarFalhaNaAutenticacao(mensagemErro: String){
+        Toast.makeText(this, mensagemErro, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun decodificarToken(token: String): Any {
+        val partes = token.split(".")
+        val payloadBase64 = partes[1]
+
+        val payloadBytes = Base64.getUrlDecoder().decode(payloadBase64)
+        val payloadJson = String(payloadBytes, StandardCharsets.UTF_8)
+
+        val json = JSONObject(payloadJson)
+        return json["idUsuario"].toString()
+    }
+
+
 }
